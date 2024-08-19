2514aad4f7960f2d12ee6809c32a5e3c75bee1ae
Thomas Perrein <perrein.thomas.tipe@gmail.com>
2024-08-19 12:08:26 +0200

    add long-lat data to tourists places

--- get_tourits_places.py
+++ get_tourits_places.py
@@ -1,9 +1,29 @@
 import dlt
 import pandas as pd
+from dlt.sources.helpers import requests
+from dotenv import load_dotenv
+import os
+
+# Charger le contenu du fichier .env
+load_dotenv()
 
 owid_disasters_csv = "./tourists_places.csv"
 df = pd.read_csv(owid_disasters_csv, sep=";")
 data = df.to_dict(orient="records")
+
+API_KEY = os.getenv("API_KEY_GEOCODE")
+
+def get_tourists_places():
+    for place in data:
+        url = f"https://api.geoapify.com/v1/geocode/search?text={place["Adresse"]}&apiKey={API_KEY}"
+        response = requests.get(url)
+        response.raise_for_status()
+        page_json = response.json()
+        if page_json:
+            yield page_json
+        else:
+            break
+
 
 pipeline = dlt.pipeline(
     pipeline_name="test",
@@ -11,7 +31,7 @@ pipeline = dlt.pipeline(
     dataset_name="public",
 )
 load_info = pipeline.run(
-    data, table_name="tourists_places", write_disposition="replace"
+    get_tourists_places(), table_name="tourists_places", write_disposition="replace"
 )
 
 print(load_info)
